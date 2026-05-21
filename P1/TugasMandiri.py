157b6f2c999c956d80cad43972a4b1fb216c59cc
hivicode <bintangazzuri08@gmail.com>
2026-05-21 16:07:51 +0700

    edit

--- P1/TugasMandiri.py
+++ P1/TugasMandiri.py
@@ -1,24 +1,28 @@
 import os 
 def buat_folder(P1): 
-    if not os.path.exists(P1): 
+    folder_sudah_ada = os.path.exists(P1)
+    print(f"Validasi sebelum folder dibuat ({P1}) : {folder_sudah_ada}")
+
+    if not folder_sudah_ada: 
         os.mkdir(P1) 
         print(f"Folder '{P1}' berhasil dibuat.") 
     else: 
         print(f"Folder '{P1}' sudah ada.") 
  
 def simpan_data(nama_file, isi_teks): 
+    file_sudah_ada = os.path.exists(nama_file)
+    print(f"Validasi sebelum file dibuat ({nama_file}) : {file_sudah_ada}")
+
     with open(nama_file, 'a') as f: 
         f.write(isi_teks + "\n") 
-    print(f"Data berhasil disimpan ke {nama_file} : {os.path.exists(nama_file)}") 
+    print(f"Data berhasil disimpan ke {nama_file}") 
 
 
 for i in range(1, 6):
-    print(f"Proses folder : {os.path.exists(f'Data_Scraping_{i}')}")
     nama_folder = f"Data_Scraping_{i}"
     buat_folder(nama_folder)
 
     for j in range(1, 6):
-        print(f"Proses file : {os.path.exists(f'{nama_folder}/hasil_{j}.txt')}")
         nama_file = f"{nama_folder}/hasil_{j}.txt"
         isi_teks = f"contoh hasil scrapping folder {i} file {j}"
         simpan_data(nama_file, isi_teks)
