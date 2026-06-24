de4d19da1608dfef716d8b6705231c905ce3d20d
ijeny <imtinan.jmb6628@gmail.com>
2026-06-24 22:39:17 +0700

    up

--- P8/scraping/pemicuScraper.bat
+++ P8/scraping/pemicuScraper.bat
@@ -1,23 +1,11 @@
-@echo off 
+@echo off
+cd /d "%~dp0"
 
-:: Mengatur direktori kerja ke lokasi file bat ini berada 
+echo Menjalankan Script Otomasi Scraper STIKOM...
 
-cd /d "%~dp0" 
+call ..\..\.venv\Scripts\activate
+python index.py
+deactivate
 
-echo Menjalankan Script Otomasi Scraper STIKOM... 
-
-:: Mengaktifkan virtual environment (pastikan folder venv ada di dalam folder yang sama) 
-
-call venv\Scripts\activate 
-
-:: Menjalankan file Python 
-
-python index.py 
-
-:: Menutup virtual environment 
-
-deactivate 
-
-echo Selesai! 
-
-Pause 
+echo Selesai!
+pause
